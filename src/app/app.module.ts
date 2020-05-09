a56e9fb9d0b4a1d24efd36217a4186121c26ad42
dantru859 <dtrujillo859@gmail.com>
2020-05-08 19:43:05 -0700

    finished lab assignment 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClient,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
